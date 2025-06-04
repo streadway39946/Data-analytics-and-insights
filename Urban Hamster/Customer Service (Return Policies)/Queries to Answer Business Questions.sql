6bb3e85bd5a2ccded89f8121531b7c139e5788b0
streadway39946 <scenario93@hotmail.com>
2025-06-04 17:55:37 -0500

    Update Queries to Answer Business Questions.sql

--- Urban Hamster/Customer Service (Return Policies)/Queries to Answer Business Questions.sql
+++ Urban Hamster/Customer Service (Return Policies)/Queries to Answer Business Questions.sql
@@ -22,7 +22,12 @@ FROM
 
 
 -- How does this return rate compare to the industry average?
--- (Not found in the database.)
+
+-- (Not found in the database, but answered below in a text block.)
+
+-- According to the National Retail Federation, in 2023 the total return rate for the retail industry as a percentage of sales was 14.5% ("2023 Consumer Returns"). 
+-- Urban Hamster's overall return rate is 10.0%, and in 2023 it was 10.3%. 
+-- These are both lower than the industry average.
 
 
 -- Has the return rate increased between 2019 and 2023?
@@ -87,4 +92,14 @@ WHERE
 -- We recognize that we have a strict return policy. How might that policy be
 -- impacting our sales, reputation, and return rate? Given those impacts, what
 -- changes would you recommend to that policy?
--- (Not found in the database.)
+
+-- (Not found in the database, but answered below in a text block.)
+
+-- Urban Hamster's return policy is strict compared to competitors like Abercrombie & Fitch and American Eagle. Urban Hamster's policy says items must be returned within 14 days of receipt. 
+-- Abercrombie & Fitch specifies 30 days ("Abercrombie"), while American Eagle states 30 days for a full refund or 60 days for a merchandise credit ("American").
+
+-- A shorter time frame for accepting returns might discourage potential customers from buying Urban Hamster products in the first place, decreasing sales. Customers who do purchase products might also not be aware that Urban Hamster only accepts returns within 14 days, and this may lead to dissatisfaction and a hit to Urban Hamster's reputation. 
+-- However, this likely has an influence on the lower return rate compared to the retail market as a whole, which is stated by the National Retail Foundation ("2023 Consumer Returns").
+
+-- I am recommending to extend the time frame of accepting returns from 14 to 30 days, to match those of competitors like Abercrombie & Fitch and American Eagle. 
+-- After extending the policy to 30 days, monitor sales, return rates, and customer reviews for changes.
